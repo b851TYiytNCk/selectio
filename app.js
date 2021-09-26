609e4f09cff9d7e24976523185dc330f3a3074f9
b851TYiytNCk <highpassion90@gmail.com>
2021-09-26 23:52:01 +0300

    add version 2 with full interactivity

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+function canRestHere(el, selectHeight) {
+	let bot = window.innerHeight - el.getBoundingClientRect().bottom;
+	return bot > selectHeight;
+} 
+
 document.addEventListener("DOMContentLoaded", function() {
 	
 	const select = document.querySelector('.select');
 	const items = document.querySelectorAll('.select ul li');
+	const list = document.querySelector('.select > ul');
 
 	['click', 'touchstart'].forEach( function(event) {
 		window.addEventListener(event, function(e) {
 			
 			if ( e.target === select && !(select.classList.contains('is-open')) ) {
 				
+				if( !canRestHere(select, 330) ) {
+					list.classList.add('inverted');
+				} else {
+					list.classList.remove('inverted');
+				}
+
 				select.classList.add('is-open');
+				
 
 			}
 
@@ -29,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
 			} else {
 				
 				if ( select.classList.contains('is-open') ) select.classList.remove('is-open');
-				
+
 			}
 
 		});
